c39beabde941d8cf6b4b03cc3917247839b2f8ce
Renat Aubakirov <arenat98@mail.ru>
2023-09-27 13:13:31 +0700

    12

---
+++ dz10/Program.cs
@@ -0,0 +1,13 @@
+﻿int SecondNumber(int num)
+{
+    return (num % 100)/ 10;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (99 < number && number < 1000)
+{
+    int number2 = SecondNumber(number);
+    Console.WriteLine($"{number} -> {number2}");
+}
+else Console.WriteLine ("Ошибка ввода");
---
+++ dz15/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine ("Введите число обозначающее день недели:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 5)
+{
+    Console.WriteLine ($"{number} -> Нет");
+}
+else if (number > 5 && number < 8)
+{
+    Console.WriteLine ($"{number} -> Да");
+}
+else if(number > 7)
+{
+    Console.WriteLine ($"{number} -> Неккоректный ввод");
+}
